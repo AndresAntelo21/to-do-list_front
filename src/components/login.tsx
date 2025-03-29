2616fcc3159e0fbeceab465690161763e11919f0
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-03-29 04:01:54 -0700

    Replace Welcome component with Login component in App and remove Welcome component

---
+++ src/components/login.tsx
@@ -0,0 +1,14 @@
+
+export const Login = () => {
+    return (
+        <div className=" relative flex flex-col items-center justify-center h-[100svh] overflow-hidden">
+            <div className="relative" >
+                <div className="absolute size-96 -z-10 blur-3xl bg-blue-700 rounded-full -bottom-10 -right-10"></div>
+                <div className="absolute size-96 -z-10 blur-3xl bg-blue-700 rounded-full -left-20"></div>
+            </div>
+            <div>
+                <h1 className="text-5xl font-poppins font-semibold">TO-DO LIST</h1>
+            </div>
+        </div>
+    )
+}
