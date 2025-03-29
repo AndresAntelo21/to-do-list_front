1bb8e1eb55ec343cbcaf1289c90df18a1cac4832
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-03-28 19:07:15 -0700

    Remove Login route from App component as it is no longer needed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "@/components/welcome";
-import { Login } from "@/components/login";
 export const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
