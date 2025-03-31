723493f9353d13f0e5e518c73a3073a8e42b4f45
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-03-30 23:48:18 -0700

    Enhance button hover effects in Login component for better user experience

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -11,14 +11,14 @@ export const Login = () => {
                 <h1 className="text-5xl font-poppins font-semibold">TO-DO LIST</h1>
             </div>
             <div className="flex flex-col gap-4">
-                <Button className="w-80 p-7 bg-white flex gap-7 rounded-3xl">
-                   <img src="/icons/google.svg" alt="logo de google" className="size-8" />
-                   <span className="text-gray-800 font-poppins text-lg">Login with Google</span>
+                <Button className="w-80 p-7 bg-white flex gap-7 rounded-3xl hover:bg-white">
+                    <img src="/icons/google.svg" alt="logo de google" className="size-8" />
+                    <span className="text-gray-800 font-poppins text-lg">Login with Google</span>
                 </Button>
 
-                <Button className="w-80 p-7 bg-black flex gap-7 rounded-3xl">
-                   <img src="/icons/github.svg" alt="logo de github" className="size-8" />
-                   <span className="text-white-800 font-poppins text-lg">Login with GitHub</span>
+                <Button className="w-80 p-7 bg-black flex gap-7 rounded-3xl hover:bg-black">
+                    <img src="/icons/github.svg" alt="logo de github" className="size-8" />
+                    <span className="text-white-800 font-poppins text-lg ">Login with GitHub</span>
                 </Button>
             </div>
         </div>
