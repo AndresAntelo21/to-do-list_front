0773412df112ca86b3fc4f65d10b862a2e04b4e0
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-04-04 18:30:18 -0700

    Comment out ProgressDay and InProgress components in Home for future reference

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,7 +1,7 @@
 import { Navigator } from "@/components/navigator/navigator";
 import { ActionBar } from "../action-bar/action-bar";
-import { ProgressDay } from "../progress/progress-day";
-import { InProgress } from "./in-progress/in-progress";
+// import { ProgressDay } from "../progress/progress-day";
+// import { InProgress } from "./in-progress/in-progress";
 // import { ProgressDay } from "@/components/progress/progress-day";
 // import { ActionBar } from "@/components/action-bar/action-bar";
 export const Home = () => {
@@ -9,8 +9,8 @@ export const Home = () => {
         <div className="flex flex-col" >
             <Navigator />
             <main className="flex flex-col p-4 gap-4 h-auto mb-70">
-                <ProgressDay />
-                <InProgress />
+                {/* <ProgressDay />
+                <InProgress /> */}
             </main>
             <ActionBar />
         </div>
